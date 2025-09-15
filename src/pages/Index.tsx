d1759df0af984e0d6770ecba4583dcfc9e02b389
Vishal chennuri <vishalnetha7@gmail.com>
2025-09-15 09:28:58 +0530

    add ons

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,4 @@
 import Layout from '@/components/layout/Layout';
-import HeroSection from '@/components/sections/HeroSection';
 import CategoryGrid from '@/components/sections/CategoryGrid';
 import FeaturedProducts from '@/components/sections/FeaturedProducts';
 import TestimonialsSection from '@/components/sections/TestimonialsSection';
@@ -8,7 +7,6 @@ import Cart from '@/components/ui/cart';
 const Index = () => {
   return (
     <Layout>
-      <HeroSection />
       <CategoryGrid />
       <FeaturedProducts />
       <TestimonialsSection />
